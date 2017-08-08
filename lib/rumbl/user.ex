d1f0cdae7254a59a784479084a618abc96f3a1eb
Ashish Singh <aitashish173@gmail.com>
2017-08-08 16:09:45 +0530

    Changes:-
    * Added migrations for creating user table
    * Added new resources routes
    * Added user template

--- lib/rumbl/user.ex
+++ lib/rumbl/user.ex
@@ -1,3 +1,22 @@
 defmodule Rumbl.User do
-  defstruct [:id, :name, :username, :password]
+  use RumblWeb, :model
+
+  schema "users" do
+    field :name,          :string
+    field :username,      :string
+    field :password,      :string, virtual: true
+    field :password_hash, :string
+
+    timestamps
+  end
+
+  #CHANGE: Passing :empty is deprecated, use :invalid instead
+  def changeset(model, params \\ :invalid) do
+    model
+    #CHANGE: cast/4 is deprecates, use cast/3 like below
+    |> cast(params, [:name, :username])
+    #CHANGE: Use validate required for presence
+    |> validate_required([:name, :username])
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -2,14 +2,21 @@ defmodule RumblWeb.UserController do
   use RumblWeb, :controller
 
   alias Rumbl.Repo
+  alias Rumbl.User
 
   def index(conn, _params) do
-    users = Repo.all_v(Rumbl.User)
+    # users = Repo.all_v(Rumbl.User)
+    users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
   end
 
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
   def show(conn, %{"id" => id}) do
-    user = Repo.get_v(Rumbl.User, id)
+    user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
   end
 end
