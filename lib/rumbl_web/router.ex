43596ac4382f2726e3d1da327bccf58d065f9f3d
Ashish Singh <aitashish173@gmail.com>
2017-08-10 17:09:55 +0530

    Added authentication
    * Login/Logout & Register with header links

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -16,9 +16,10 @@ defmodule RumblWeb.Router do
 
   scope "/", RumblWeb do
     pipe_through :browser # Use the default browser stack
-
-    resources "/users", UserController, only: [:index, :show, :new, :create]
+    
     get "/",            PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
   # Other scopes may use custom stacks.
